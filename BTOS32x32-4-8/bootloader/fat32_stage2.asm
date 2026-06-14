f6aecfa6be0f605b725e623c354d6ee80a6e6b16
NguyenQuocLien <liennguyenquoc9@gmail.com>
2026-06-14 16:17:09 +0700

    Update fat32_stage2.asm

--- BTOS32x32-4-8/bootloader/fat32_stage2.asm
+++ BTOS32x32-4-8/bootloader/fat32_stage2.asm
@@ -1,3 +1,6 @@
+; =============================================
+; FAT32 Stage2 - Đọc kernel từ ổ đĩa (ATA PIO)
+; =============================================
 BITS 32
 global load_kernel_from_fat32
 
@@ -5,19 +8,67 @@ load_kernel_from_fat32:
     push ebp
     mov ebp, esp
 
-    ; Giả lập đọc ổ đĩa cứng cổng I/O ATA để nạp Kernel
-    ; Trong thực tế, đoạn này sẽ gửi lệnh đến cổng 0x1F0 - 0x1F7 để đọc các Sector FAT32
-    mov ebx, 0x100000       ; Địa chỉ đích nạp Kernel (1MB)
-    mov ecx, 64             ; Giả định đọc 64 Sectors thô chứa file kernel.bin (32KB)
-    mov edx, 0x1F0          ; Cổng dữ liệu ATA Data Port
+    ; Đọc 128 sector (64KB) kernel từ LBA 10 trở đi
+    ; (Bạn có thể thay đổi LBA khi tạo image sau)
+    mov ebx, 0x100000       ; Địa chỉ đích
+    mov ecx, 128            ; Số sector cần đọc
+    mov edx, 0x1F0          ; ATA Data Port
 
-.read_loop:
-    ; (Đoạn mã gửi lệnh đọc sector và kiểm tra trạng thái ổ đĩa rảnh)
-    ; Đọc dữ liệu từ cổng đĩa cứng vào bộ nhớ RAM tại địa chỉ ebx
-    insw                    ; Đọc một từ (Word) từ cổng EDX vào ES:EDI/EBX
-    add ebx, 2
-    loop .read_loop
+    mov eax, 10             ; LBA bắt đầu (sector 10)
+    call ata_read_sectors
 
     mov esp, ebp
     pop ebp
+    ret
+
+; ==================== Hàm đọc sector ATA PIO ====================
+ata_read_sectors:
+    ; eax = LBA, ecx = số sector, ebx = buffer đích
+    pushad
+
+.read_loop:
+    ; Gửi lệnh đọc
+    mov dx, 0x1F2
+    mov al, 1               ; đọc 1 sector/lần
+    out dx, al
+
+    mov dx, 0x1F3
+    mov al, al              ; LBA low
+    out dx, al
+
+    mov dx, 0x1F4
+    shr eax, 8
+    out dx, al
+
+    mov dx, 0x1F5
+    shr eax, 8
+    out dx, al
+
+    mov dx, 0x1F6
+    shr eax, 8
+    or al, 0xE0             ; LBA mode + master
+    out dx, al
+
+    mov dx, 0x1F7
+    mov al, 0x20            ; READ SECTORS
+    out dx, al
+
+.wait_ready:
+    in al, dx
+    test al, 0x80           ; BSY
+    jnz .wait_ready
+    test al, 0x08           ; DRQ
+    jz .wait_ready
+
+    ; Đọc 256 word (512 byte)
+    mov dx, 0x1F0
+    mov edi, ebx
+    mov cx, 256
+    rep insw
+
+    add ebx, 512
+    dec dword [esp+28]      ; giảm ecx (số sector còn lại)
+    jnz .read_loop
+
+    popad
     ret
